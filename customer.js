08b82b97b16b0eeb9a125d7270c1302e84f15fc9
akaryrye <ralldrin@gmail.com>
2019-09-07 00:46:58 -0700

    Added purchase functionality

--- customer.js
+++ customer.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+require('events').EventEmitter.defaultMaxListeners = 25;
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -17,7 +18,7 @@ function initialPrompt() {
                 type: 'list',
                 name: "choices",
                 message: "Select from the following options:",
-                choices: ["display goods", "buy item"]
+                choices: ["display goods", "buy item", "exit"]
             }
         ])
         .then(input => {
@@ -25,6 +26,10 @@ function initialPrompt() {
                 displayGoods();
             } else if (input.choices === 'buy item') {
                 buyPrompt();
+            } else if (input.choices === 'exit') {
+                connection.end();
+                console.log("Have a nice day!")
+                process.exit(0);
             }
         });
 
@@ -34,17 +39,17 @@ function buyPrompt() {
     inquirer.prompt([
         {
             type: 'input',
-            message: 'enter the id of the item you would like to buy',
+            message: 'Please enter the item\'s ID: ',
             name: 'itemId',
         },
         {
             type: 'input',
-            message: 'enter the quantity you would like to purchase',
+            message: 'Please enter how many you would like to buy: ',
             name: 'howMany'
         }
-    ]).then( input => {
-        console.log(input);
-    })
+    ]).then(input => {
+        changeQuantity(input.itemId, input.howMany);
+    });
 }
 
 function displayGoods() {
@@ -53,11 +58,11 @@ function displayGoods() {
         for (let i = 0; i < res.length; i++) {
             console.log(
                 `------------------------------------
-ID: ${res[i].ID}
-NAME: ${res[i].NAME}
-PRICE: $ ${res[i].PRICE}.00
-QUANTITY: ${res[i].QUANTITY}
-DESCRIPTION: ${res[i].DESCRIPTION}`
+                ID: ${res[i].id}
+                NAME: ${res[i].name}
+                PRICE: $ ${res[i].price}.00
+                QUANTITY: ${res[i].quantity}
+                DESCRIPTION: ${res[i].description}`
             );
         }
         console.log("------------------------------------");
@@ -65,21 +70,28 @@ DESCRIPTION: ${res[i].DESCRIPTION}`
     });
 }
 
-
 function changeQuantity(item, num) {
-    connection.query("SELECT quantity FROM products WHERE ID = ?", item, function (err, res, fields) {
+    connection.query("SELECT id, name, price, quantity FROM products WHERE ID = ?", item, function (err, res, fields) {
         if (err) throw err;
-        console.log(res)
-        if (res.quantity < num) {
-            return console.log("There are not enough items for this purchase")
+        console.log("\n==============\n")
+        console.log(`Item Selected:\n   ID: ${res[0].id}\n   Name: ${res[0].name}\n   Price(ea): ${res[0].price}\n   Quantity Available: ${res[0].quantity}`);
+
+        if (res[0].quantity < num) {
+            console.log("\nInsufficient stock for this purchase");
+            console.log("\n==============\n")
+            initialPrompt();
         } else {
-            let newQuantity = parseInt(res.quantity) - num;
-            console.log("New Quantity: " + newQuantity);
-            //connection.query("")
+            let newQuantity = res[0].quantity - num;
+            let purchasePrice = num * parseFloat(res[0].price);
+            connection.query("UPDATE products SET quantity = ? WHERE ID = ?", [newQuantity, item], function (error, response, fields) {
+                if (error) throw error;
+                console.log(`\nYou have successfuly purchased ${num} ${res[0].name}'s for the price of $ ${purchasePrice}.00`);
+                console.log("\n==============\n")
+                initialPrompt();
+            });
         }
-        initialPrompt();
     });
 }
 
-// Entry Point
+// Display the current stock when program opens
 displayGoods();
